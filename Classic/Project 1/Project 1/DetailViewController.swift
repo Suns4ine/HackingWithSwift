0b4a2e18417090e95892f65931cb97cf5f3f2f60
Suns4ine <pronin.slavok@mail.ru>
2020-06-06 23:07:26 +0300

    new interface title and view  pictures

---
+++ Classic/Project 1/Project 1/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  Project 1
+//
+//  Created by Vyacheslav Pronin on 06/06/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToload = selectedImage {
+            imageView.image = UIImage(named: imageToload)
+        }
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Classic/Project 1/Project 1/ViewController.swift
+++ Classic/Project 1/Project 1/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UITableViewController {
     var pictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
 
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
@@ -36,6 +39,13 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
 
