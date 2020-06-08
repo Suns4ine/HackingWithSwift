32a86ce6d5e068907c335b3fc334ed779d13cb92
Suns4ine <pronin.slavok@mail.ru>
2020-06-08 23:15:55 +0300

    New Project 2: Add buttons and colors in viewController

---
+++ Classic/Project 2/Project 2/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Vyacheslav Pronin on 08/06/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        // Do any additional setup after loading the view.
+        askQuestion()
+    }
+    func askQuestion(){//action: UIApplication! = nil) {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//    }
+
+
+}
+
