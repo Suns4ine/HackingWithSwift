6d17171f015b614aea8ed4ed6a6a4fffd5f297c6
Suns4ine <pronin.slavok@mail.ru>
2020-06-16 02:59:12 +0300

    image on table and small fix

--- Classic/Consolidation 2/Consolidation 2/DetailViewController.swift
+++ Classic/Consolidation 2/Consolidation 2/DetailViewController.swift
@@ -13,10 +13,14 @@ class DetailViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
     var selectedImage: String?
+    var text = ""
+    let appName = Bundle.appName()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         navigationItem.largeTitleDisplayMode = .never
         
         if let imageToLoad = selectedImage {
@@ -36,6 +40,34 @@ class DetailViewController: UIViewController {
                 super.viewWillDisappear(animated)
                 navigationController?.hidesBarsOnTap = false
             }
-    
+    @objc func shareTapped() {
+        
+        
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        text = """
+This is \(selectedImage?.capitalized ?? "No name found")
+I sent it through "\(appName)" application
+"""
 
+        let vc = UIActivityViewController(activityItems: [text, image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+}
+
+extension Bundle {
+    static func appName() -> String {
+        guard let dictionary = Bundle.main.infoDictionary else {
+            return ""
+        }
+        if let version : String = dictionary["CFBundleName"] as? String {
+            return version
+        } else {
+            return ""
+        }
+    }
 }
--- Classic/Consolidation 2/Consolidation 2/ViewController.swift
+++ Classic/Consolidation 2/Consolidation 2/ViewController.swift
@@ -31,16 +31,18 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Countries", for: indexPath)
         cell.textLabel?.text = countries[indexPath.row].capitalized
+        cell.imageView?.image = UIImage(named: countries[indexPath.row])
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            // 2: success! Set its selectedImage property
+            
+
             vc.selectedImage = countries[indexPath.row]
 
-            // 3: now push it onto the navigation controller
             navigationController?.pushViewController(vc, animated: true)
         }
     }
