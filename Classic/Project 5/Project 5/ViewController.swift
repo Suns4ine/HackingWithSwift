1a2ee119f2324a1280da90143ea0df910093a52e
Suns4ine <pronin.slavok@mail.ru>
2020-07-15 04:11:45 +0300

    new button 'New Game' and fix bug for submit

--- Classic/Project 5/Project 5/ViewController.swift
+++ Classic/Project 5/Project 5/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "New Game", style: .plain, target: self, action: #selector(startGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -31,7 +32,7 @@ class ViewController: UITableViewController {
         
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -60,6 +61,7 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
+    
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
 
@@ -67,6 +69,14 @@ class ViewController: UITableViewController {
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
+                    for word in usedWords {
+                    
+                        if answer.lowercased() == word.lowercased() {
+                            showErrorMessage(error: "isOriginal")
+                            return
+                        }
+                    }
+                    
                     usedWords.insert(answer, at: 0)
 
                     let indexPath = IndexPath(row: 0, section: 0)
