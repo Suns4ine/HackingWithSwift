55b0bd753e1be50e10aa5a595f9426c1e13c0319
Suns4ine <pronin.slavok@mail.ru>
2020-07-19 02:46:14 +0300

    add multiplayer in constraint

--- Classic/Project 6b/Project 6b/ViewController.swift
+++ Classic/Project 6b/Project 6b/ViewController.swift
@@ -59,13 +59,22 @@ class ViewController: UIViewController {
 //        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewDictionary))
 //
         var previous: UILabel?
+        var arrayLabel = [label1, label2, label3, label4, label5]
         
-        for label in [label1, label2, label3, label4, label5] {
-            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+        let heightLabel = (UIScreen.main.bounds.height - 50)/5
+        
+        for label in arrayLabel {
+            
+            //label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            //label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            //label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 88).isActive = true
+            label.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.5).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.19).isActive = true
+            //label.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5, constant: 50).isActive = true
+           // label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor).isActive = true
             
             if let previous = previous {
-                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+                label.topAnchor.constraint(equalTo: previous.safeAreaLayoutGuide.bottomAnchor, constant: 10).isActive = true
             } else {
                 label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
             }
