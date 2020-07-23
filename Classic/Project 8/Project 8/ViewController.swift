7873721aa7e7fe5986706c964e60aae5678bce78
Suns4ine <pronin.slavok@mail.ru>
2020-07-23 23:38:09 +0300

    add Lables: answer and clues. Add currentanswer and autolayot

--- Classic/Project 8/Project 8/ViewController.swift
+++ Classic/Project 8/Project 8/ViewController.swift
@@ -19,6 +19,47 @@ class ViewController: UIViewController {
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        cluesLabel = UILabel()
+        cluesLabel.translatesAutoresizingMaskIntoConstraints = false
+        cluesLabel.font = UIFont.systemFont(ofSize: 24)
+        cluesLabel.text = "CLUES"
+        cluesLabel.numberOfLines = 0
+        view.addSubview(cluesLabel)
+        
+        answersLbel = UILabel()
+        answersLbel.translatesAutoresizingMaskIntoConstraints = false
+        answersLbel.font = UIFont.systemFont(ofSize: 24)
+        answersLbel.text = "ANSWER"
+        answersLbel.numberOfLines = 0
+        answersLbel.textAlignment = .right
+        view.addSubview(answersLbel)
+        
+        currentAnswer = UITextField()
+        currentAnswer.translatesAutoresizingMaskIntoConstraints = false
+        currentAnswer.placeholder = "Tap letters to guess"
+        currentAnswer.textAlignment = .center
+        currentAnswer.font = UIFont.systemFont(ofSize: 44)
+        currentAnswer.isUserInteractionEnabled = false
+        view.addSubview(currentAnswer)
+
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 0),
+            cluesLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+            cluesLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
+            cluesLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.6, constant: -100),
+            answersLbel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+            answersLbel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -100),
+            answersLbel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.4, constant: -100),
+            answersLbel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor),
+        ])
     }
     
     override func viewDidLoad() {
