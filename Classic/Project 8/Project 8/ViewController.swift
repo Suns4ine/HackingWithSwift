e40647d6daeb0cc79676cfa46043bd95fdab629c
Suns4ine <pronin.slavok@mail.ru>
2020-07-25 01:42:58 +0300

    added funcs letter, submit clear. new func levelUp

--- Classic/Project 8/Project 8/ViewController.swift
+++ Classic/Project 8/Project 8/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
     var activatedButtons = [UIButton]()
     var solutions = [String]()
     
-    var score = 0
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     var level = 1
 
     override func loadView() {
@@ -127,19 +132,46 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadLevel()
         // Do any additional setup after loading the view.
     }
 
     @objc func letterTapped(_ sender: UIButton) {
-        
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
     }
     
     @objc func submitTapped(_ sender: UIButton) {
+        guard let answerText = currentAnswer.text else { return }
         
+        if let solutionPosition = solutions.firstIndex(of: answerText) {
+            activatedButtons.removeAll()
+            
+            var splitAnswer = answersLabel.text?.components(separatedBy: "\n")
+            splitAnswer?[solutionPosition] = answerText
+            answersLabel.text = splitAnswer?.joined(separator: "\n")
+            
+            currentAnswer.text = ""
+            score += 1
+            
+            if score % 7 == 0 {
+                let ac = UIAlertController(title: "Well done!", message: "Are you ready for the next level?", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Let's go!", style: .default, handler: levelUp))
+                present(ac, animated: true)
+            }
+        }
     }
     
     @objc func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
         
+        for btn in activatedButtons {
+            btn.isHidden = true
+        }
+        
+        activatedButtons.removeAll()
     }
     
     func loadLevel() {
@@ -149,25 +181,47 @@ class ViewController: UIViewController {
         
         if let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
             if let levelContents = try? String(contentsOf: levelFileURL) {
-                    var lines = levelContents.components(separatedBy: "\n")
-                    lines.shuffle()
-
-                    for (index, line) in lines.enumerated() {
-                        let parts = line.components(separatedBy: ": ")
-                        let answer = parts[0]
-                        let clue = parts[1]
-
-                        clueString += "\(index + 1). \(clue)\n"
-
-                        let solutionWord = answer.replacingOccurrences(of: "|", with: "")
-                        solutionString += "\(solutionWord.count) letters\n"
-                        solutions.append(solutionWord)
-
-                        let bits = answer.components(separatedBy: "|")
-                        letterBits += bits
+                var lines = levelContents.components(separatedBy: "\n")
+                lines.shuffle()
+                
+                for (index, line) in lines.enumerated() {
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString += "\(index + 1). \(clue)\n"
+                    
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.count) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
                     }
                 }
             }
+        
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        letterBits.shuffle()
+        
+        if letterBits.count == letterButtons.count {
+            for i in 0..<letterButtons.count {
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+            }
         }
+    
+    func levelUp(action: UIAlertAction) {
+        level += 1
+        solutions.removeAll(keepingCapacity: true)
+        
+        loadLevel()
+        
+        for btn in letterButtons {
+            btn.isHidden = false
+        }
+    }
 }
 
