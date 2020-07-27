d570db72105498a80dbae70fd202ae224d16c580
Suns4ine <pronin.slavok@mail.ru>
2020-07-27 23:25:35 +0300

    New Consolidation: Game Hangman

---
+++ Classic/Consalidation 4/Consalidation 4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Consalidation 4
+//
+//  Created by Vyacheslav Pronin on 27/07/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
