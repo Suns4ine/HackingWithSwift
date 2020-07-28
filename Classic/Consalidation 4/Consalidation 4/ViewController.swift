f5889fe7a69d91a8ebc346f5603c5f81a36a5ed5
Suns4ine <pronin.slavok@mail.ru>
2020-07-28 03:22:44 +0300

    Add Lables and Buttons. Code Autolayot

--- Classic/Consalidation 4/Consalidation 4/ViewController.swift
+++ Classic/Consalidation 4/Consalidation 4/ViewController.swift
@@ -10,8 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lifeLabel: UILabel!
+    var wordLabel: UILabel!
+    var textButton: UIButton!
+    var newGameButton: UIButton!
+    
+    var point = 10 {
+        didSet {
+            lifeLabel.text = "Your point to life: \(point)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view = UIView()
+        view.backgroundColor = .white
+        
+        lifeLabel = UILabel()
+        lifeLabel.translatesAutoresizingMaskIntoConstraints = false
+        lifeLabel.text = "Your point to life: \(point)"
+        view.addSubview(lifeLabel)
+        
+        wordLabel = UILabel()
+        wordLabel.translatesAutoresizingMaskIntoConstraints = false
+        wordLabel.text = "***"
+        view.addSubview(wordLabel)
+        
+        textButton = UIButton(type: .system)
+        textButton.translatesAutoresizingMaskIntoConstraints = false
+        textButton.setTitle("submit", for: .normal)
+        view.addSubview(textButton)
+        
+        newGameButton = UIButton(type: .system)
+        newGameButton.translatesAutoresizingMaskIntoConstraints = false
+        newGameButton.setTitle("New Game", for: .normal)
+        view.addSubview(newGameButton)
+        
+        NSLayoutConstraint.activate([
+            lifeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
+            wordLabel.topAnchor.constraint(equalTo: lifeLabel.topAnchor, constant: 40),
+            textButton.topAnchor.constraint(equalTo: wordLabel.topAnchor, constant: 40),
+            newGameButton.topAnchor.constraint(equalTo: textButton.topAnchor, constant: 40)
+        ])
         // Do any additional setup after loading the view.
     }
 
