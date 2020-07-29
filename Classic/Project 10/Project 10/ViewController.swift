f8c3e1f1926f8f1cee30fa214b9fe5939d43f97e
Suns4ine <pronin.slavok@mail.ru>
2020-07-29 23:51:01 +0300

    New Project 10: Book with names

---
+++ Classic/Project 10/Project 10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 10
+//
+//  Created by Vyacheslav Pronin on 29/07/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
