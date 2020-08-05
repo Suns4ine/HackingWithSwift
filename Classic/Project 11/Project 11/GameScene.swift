c95a314301b2413dd21a8a56104d2490be46f548
Suns4ine <pronin.slavok@mail.ru>
2020-08-05 23:42:18 +0300

    Add coda in didMove and touchesBegan. Add phisic

--- Classic/Project 11/Project 11/GameScene.swift
+++ Classic/Project 11/Project 11/GameScene.swift
@@ -10,13 +10,21 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
-    }
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+            box.position = location
+            addChild(box)
+        }
     }
 }
