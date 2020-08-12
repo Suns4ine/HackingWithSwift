4360e866134907e2982d0e443537ba04eaad445d
Suns4ine <pronin.slavok@mail.ru>
2020-08-12 23:32:41 +0300

    New Consolidation: Ambum

---
+++ Classic/Consolidation 5/Consolidation 5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Consolidation 5
+//
+//  Created by Vyacheslav Pronin on 12/08/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
