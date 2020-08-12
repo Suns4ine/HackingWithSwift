35037c3d095b4ed76575da5ce91a839ac976c4dd
Suns4ine <pronin.slavok@mail.ru>
2020-08-13 02:34:25 +0300

    New File Photo, add navigatorItem and func add

---
+++ Classic/Consolidation 5/Consolidation 5/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Consolidation 5
+//
+//  Created by Vyacheslav Pronin on 13/08/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import Foundation
+
+class Photo: NSObject, Codable {
+  var name: String
+  var image: String
+}
--- Classic/Consolidation 5/Consolidation 5/ViewController.swift
+++ Classic/Consolidation 5/Consolidation 5/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var photos = [Photo]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Photo", for: indexPath)
+        cell.textLabel?.text = photos[indexPath.row].name
+        return cell
+    }
+    
+    @objc func add() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.camera) { picker.sourceType = .camera }
+        
+        present(picker, animated: true)
+    }
 
 }
 
