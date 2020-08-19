2fa78c4043db2cdf03ae1f97fefe8dbf928a3707
Suns4ine <pronin.slavok@mail.ru>
2020-08-19 03:19:58 +0300

    Add slider for radius

--- Classic/Project 13/Project 13/ViewController.swift
+++ Classic/Project 13/Project 13/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
+    @IBOutlet var radius: UISlider!
     
     var currentImage: UIImage!
     var context: CIContext!
@@ -53,8 +54,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(ac, animated: true)
     }
     
+    @IBAction func radiusChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    
     @IBAction func save(_ sender: Any) {
-        guard let image = imageView.image else {
+        guard imageView.image != nil else {
             let ac = UIAlertController(title: "Oops", message: "You didn't choose choosi in image.", preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "OK", style: .default))
             
@@ -81,7 +87,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let inputKeys = currentFilter.inputKeys
         
          if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
-           if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
+           if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(radius.value * 200, forKey: kCIInputRadiusKey) }
            if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
            if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
         
