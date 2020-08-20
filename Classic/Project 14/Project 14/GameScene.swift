3960c4510242dc4a8d70e2dd34d8a9937975b03d
Suns4ine <pronin.slavok@mail.ru>
2020-08-20 03:11:08 +0300

    Add funcs createSlot and configure. Add figures cropNode and charNode (pinguin).

--- Classic/Project 14/Project 14/GameScene.swift
+++ Classic/Project 14/Project 14/GameScene.swift
@@ -12,6 +12,7 @@ import GameplayKit
 class GameScene: SKScene {
     
     var gameScore: SKLabelNode!
+    var slots = [WhackSlot]()
     var score = 0 {
         didSet {
             gameScore.text = "Score \(score)"
@@ -36,9 +37,21 @@ class GameScene: SKScene {
         gameScore.fontSize = 48
         addChild(gameScore)
         
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 410))}
+        for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320))}
+        for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230))}
+        for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140))}
+           
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
      
+    }
+    
+    func createSlot(at position: CGPoint) {
+        let slot = WhackSlot()
+        slot.configure(at: position)
+        addChild(slot)
+        slots.append(slot)
     }
 }
