d463e57b6cb7ea870bccefe3cc928fd915caebfe
Suns4ine <pronin.slavok@mail.ru>
2020-08-21 22:46:20 +0300

    Add GCD and func createEnemy

--- Classic/Project 14/Project 14/GameScene.swift
+++ Classic/Project 14/Project 14/GameScene.swift
@@ -43,6 +43,9 @@ class GameScene: SKScene {
         for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230))}
         for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140))}
            
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -54,6 +57,27 @@ class GameScene: SKScene {
         slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
+    }
+    
+    func createEnemy() {
+    popupTime *= 0.991
+
+    slots.shuffle()
+    slots[0].show(hideTime: popupTime)
+
+    if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
+    if Int.random(in: 0...12) > 8 {  slots[2].show(hideTime: popupTime) }
+    if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
+    if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime)  }
+
+    let minDelay = popupTime / 2.0
+    let maxDelay = popupTime * 2
+    let delay = Double.random(in: minDelay...maxDelay)
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+        self?.createEnemy()
+        }
+        
     }
     
 }
