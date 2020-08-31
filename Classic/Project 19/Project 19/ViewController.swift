c3930d8a4c71b798c19304553f8ef9f9d649976a
Suns4ine <pronin.slavok@mail.ru>
2020-08-31 21:38:50 +0300

    New Project 19: Extension

---
+++ Classic/Project 19/Project 19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 19
+//
+//  Created by Vyacheslav Pronin on 31/08/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
