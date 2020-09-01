7b8fd8f4ef71b2c6d08f6c8d91f2195decd1431b
Suns4ine <pronin.slavok@mail.ru>
2020-09-01 17:21:34 +0300

    Add UIAlertController and UserDefaults

--- Classic/Project 19/Extension/ActionViewController.swift
+++ Classic/Project 19/Extension/ActionViewController.swift
@@ -12,17 +12,31 @@ import MobileCoreServices
 class ActionViewController: UIViewController {
 
     @IBOutlet var script: UITextView!
+    var arrrayURLJavaScript = [ URL : String ]()
     
     var pageTitle = ""
     var pageURL = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        let defaults = UserDefaults.standard
+        
+        if let savedArray = defaults.object(forKey: "arrrayURLJavaScript") as? Data {
+            if let decodedArray = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedArray) as? [URL: String] {
+                arrrayURLJavaScript = decodedArray
+                print(arrrayURLJavaScript.keys)////////////////////////
+            }
+        }
+
+        
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: .add, style: .done, target: self, action: #selector(addCode))
+        
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
@@ -41,6 +55,7 @@ class ActionViewController: UIViewController {
 
     @IBAction func done() {
         let item = NSExtensionItem()
+        save()
         let argument: NSDictionary = ["customJavascript": script.text]
         let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
         let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
@@ -65,6 +80,28 @@ class ActionViewController: UIViewController {
         
         let selectedRange = script.selectedRange
         script.scrollRangeToVisible(selectedRange)
+    }
+    
+    @objc func addCode() {
+        let vc = UIAlertController(title: "JavaScript", message: nil, preferredStyle: .alert)
+        vc.addAction(UIAlertAction(title: "Alert", style: .default) { [weak self] _ in
+            self?.script.text = "alert(document.title);"
+            self?.done()
+        })
+        vc.addAction(UIAlertAction(title: "window", style: .default){ [weak self] _ in
+            self?.script.text = "window.open('https://apple.com');"
+            self?.done()
+        })
+        vc.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(vc, animated: true)
+    }
+    
+    func save () {
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: arrrayURLJavaScript, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "arrrayURLJavaScript")
+        }
     }
 
 }
