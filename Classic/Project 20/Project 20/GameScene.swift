a03cc80fea09ccdb15ca6c61766792a9457b4521
Suns4ine <pronin.slavok@mail.ru>
2020-09-01 22:57:55 +0300

    Add funcs launchFireworks and createFireworks

--- Classic/Project 20/Project 20/GameScene.swift
+++ Classic/Project 20/Project 20/GameScene.swift
@@ -35,6 +35,72 @@ class GameScene: SKScene {
     }
     
     @objc func launchFireworks() {
+        let movementAmount: CGFloat = 1800
         
+        switch Int.random(in: 0...3) {
+        case 0:
+            createFireworks(xMovement: 0, x: 512, y: bottomEdge)
+            createFireworks(xMovement: 0, x: 512 - 200, y: bottomEdge)
+            createFireworks(xMovement: 0, x: 512 - 100, y: bottomEdge)
+            createFireworks(xMovement: 0, x: 512 + 100, y: bottomEdge)
+            createFireworks(xMovement: 0, x: 512 + 200, y: bottomEdge)
+        case 1:
+            createFireworks(xMovement: 0, x: 512, y: bottomEdge)
+            createFireworks(xMovement: -200, x: 512 - 200, y: bottomEdge)
+            createFireworks(xMovement: -100, x: 512 - 100, y: bottomEdge)
+            createFireworks(xMovement: 100, x: 512 + 100, y: bottomEdge)
+            createFireworks(xMovement: 200, x: 512 + 200, y: bottomEdge)
+        case 2:
+            createFireworks(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 400)
+            createFireworks(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 300)
+            createFireworks(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 200)
+            createFireworks(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 100)
+            createFireworks(xMovement: movementAmount, x: leftEdge, y: bottomEdge)
+        case 3:
+            createFireworks(xMovement: -movementAmount, x: righitEdge, y: bottomEdge + 400)
+            createFireworks(xMovement: -movementAmount, x: righitEdge, y: bottomEdge + 300)
+            createFireworks(xMovement: -movementAmount, x: righitEdge, y: bottomEdge + 200)
+            createFireworks(xMovement: -movementAmount, x: righitEdge, y: bottomEdge + 100)
+            createFireworks(xMovement: -movementAmount, x: righitEdge, y: bottomEdge)
+        default:
+            break
+        }
+    }
+    
+    func createFireworks(xMovement: CGFloat, x: Int, y: Int) {
+        
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.colorBlendFactor = 1
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        switch Int.random(in: 0...2) {
+        case 0:
+            firework.color = .cyan
+        case 1:
+            firework.color = .green
+        case 2:
+            firework.color = .red
+        default:
+            break
+        }
+        
+        let path = UIBezierPath()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: xMovement, y: 1000))
+        
+        let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: true, speed: 200)
+        node.run(move)
+        
+        if let emitter = SKEmitterNode(fileNamed: "fuse") {
+            emitter.position = CGPoint(x: 0, y: -22)
+            node.addChild(emitter)
+        }
+        
+        fireworks.append(node)
+        addChild(node)
     }
 }
