2727a6285827a1c6e49a5e0c188886ffa54089e3
Suns4ine <pronin.slavok@mail.ru>
2020-09-02 23:40:57 +0300

    New Project 21: Pushers

---
+++ Classic/Project 21/Project 21/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project 21
+//
+//  Created by Vyacheslav Pronin on 02/09/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @objc func registerLocal() {
+        
+    }
+    
+    @objc func sheduleLocal() {
+        
+    }
+
+}
+
