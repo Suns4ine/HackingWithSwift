420a8bceab8386e1d56cd485a8d397ea915c08b7
Suns4ine <pronin.slavok@mail.ru>
2020-09-08 10:37:01 +0300

    Add funcs touchesBegan, redrawActiveSlice and sounds in app

--- Classic/Project 23/Project 23/GameScene.swift
+++ Classic/Project 23/Project 23/GameScene.swift
@@ -11,6 +11,9 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+    
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
@@ -21,8 +24,13 @@ class GameScene: SKScene {
         }
     }
     
+    var activeSlicePoints = [CGPoint]()
     var livesImages = [SKSpriteNode]()
     var lives = 3
+    
+    var isSwooshSoundActive = false
+    
+    
     
     override func didMove(to view: SKView) {
      
@@ -42,7 +50,24 @@ class GameScene: SKScene {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        activeSlicePoints.removeAll(keepingCapacity: true)
+        
+        let location = touch.location(in: self)
+        activeSlicePoints.append(location)
+        redrawActiveSlice()
+        
+        activeSliceBG.removeAllActions()
+        activeSliceFG.removeAllActions()
+        
+        activeSliceBG.alpha = 1
+        activeSliceFG.alpha = 1
+    }
     
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        activeSliceBG.run(SKAction.fadeOut(withDuration: 0.25))
+        activeSliceFG.run(SKAction.fadeOut(withDuration: 0.25))
     }
     
     func createScore() {
@@ -55,6 +80,16 @@ class GameScene: SKScene {
         score = 0
     }
     
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        activeSlicePoints.append(location)
+        redrawActiveSlice()
+        if !isSwooshSoundActive {
+            playSwoochSound()
+        }
+    }
+    
     func createLives() {
         for i in 0..<3 {
             let spriteNode = SKSpriteNode(imageNamed: "slicelife")
@@ -65,7 +100,55 @@ class GameScene: SKScene {
         }
     }
     
-    func createSlices() {
+    func redrawActiveSlice() {
+        if activeSlicePoints.count < 2 {
+            activeSliceBG.path = nil
+            activeSliceFG.path = nil
+            return
+        }
         
+        if activeSlicePoints.count > 12 {
+            activeSlicePoints.removeFirst(activeSlicePoints.count - 12)
+        }
+        
+        let path = UIBezierPath()
+        path.move(to: activeSlicePoints[0])
+        
+        for i in 1 ..< activeSlicePoints.count {
+            path.addLine(to: activeSlicePoints[i])
+        }
+        
+        activeSliceBG.path = path.cgPath
+        activeSliceFG.path = path.cgPath
+    }
+    
+    func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
+        
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 3
+        
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+        
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
+    }
+    
+    func playSwoochSound() {
+        isSwooshSoundActive = true
+        
+        let randomNumber = Int.random(in: 1...3)
+        let soundName = "swooch\(randomNumber).caf"
+        
+        let swooshSound = SKAction.playSoundFileNamed(soundName , waitForCompletion: true)
+        
+        run(swooshSound) { [weak self] in
+            self?.isSwooshSoundActive = false
+        }
     }
 }
