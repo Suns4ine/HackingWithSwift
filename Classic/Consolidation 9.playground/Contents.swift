4f740d8e636a3331e37a0d7cc5e2d171fdcd9d06
Suns4ine <pronin.slavok@mail.ru>
2020-09-10 15:24:35 +0300

    Add extension times for Int and remove(item:) for Array

--- Classic/Consolidation 9.playground/Contents.swift
+++ Classic/Consolidation 9.playground/Contents.swift
@@ -7,3 +7,63 @@ extension UIView {
         }
     }
 }
+
+
+extension Int {
+    func times(_ clousure: ()->()) {
+        if self <= 0 { return }
+        var i = self
+        while (i > 0) {
+            clousure()
+            i -= 1
+        }
+    }
+}
+
+var i = -5
+i.times {
+    print("Hello\(i)")
+}
+
+extension Array where Element: Comparable {
+    mutating func remove(item: Element) {
+        if let itemArray = self.firstIndex(of: item) {
+            self.remove(at: itemArray)
+        }
+    }
+}
+    
+//        if self.isEmpty { return self }
+//        var newArray = self
+//        var count = 0
+//
+//        for i in newArray {
+//            if i == item {
+//                count += 1
+//            }
+//
+//            if count == 2 {
+//                break
+//            }
+//        }
+//
+//        if count >= 2 {
+//            for i in newArray {
+//                if i == item {
+//                    newArray = newArray.remove(item: i)
+//                    return newArray
+//                }
+//            }
+//        }
+//
+//        return newArray
+//    }
+
+
+var array = [1, 6, 3, 4, 5, 2, 6, 8]
+
+array.remove(item: 6)
+
+for i in array {
+    print(i)
+}
