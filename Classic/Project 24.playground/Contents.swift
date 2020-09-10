cb69fd73057dd4f99a95938aea6c9f751b7ef54c
Suns4ine <pronin.slavok@mail.ru>
2020-09-10 14:11:06 +0300

    Add extension: withPrefix, isNumeric and lines

--- Classic/Project 24.playground/Contents.swift
+++ Classic/Project 24.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 let name = "Taylor"
 
@@ -37,4 +38,69 @@ extension String {
         guard let firstLetter = self.first else { return "" }
         return firstLetter.uppercased() + self.dropFirst()
     }
+}
+
+let string = "This is a test string"
+let attributes: [NSAttributedString.Key : Any] = [
+    .foregroundColor : UIColor.white,
+    .backgroundColor : UIColor.red,
+    .font : UIFont.boldSystemFont(ofSize: 36)
+]
+
+//let attributedString = NSAttributedString(string: string, attributes: attributes)
+
+//print(attributedString)
+
+let attributedString = NSMutableAttributedString(string: string)
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
+
+
+extension String {
+    mutating func withPrefix(_ prefix: String) -> String {
+        if self.hasPrefix(prefix) { return self }
+        return String(prefix + self)
+    }
+}
+
+var first = "2first"
+first.withPrefix("1")
+
+print(first)
+
+
+extension String {
+    func isNumeric(_ numb: Double) -> Bool {
+        if !self.isEmpty && self.contains(String(numb)) {
+            return true
+        }
+        return false
+    }
+    
+    func isNumeric(_ numb: Int) -> Bool {
+        if !self.isEmpty && self.contains(String(numb)) {
+            return true
+        }
+        return false
+    }
+
+}
+first.isNumeric(2)
+
+
+extension String {
+    func lines() -> [String] {
+        var array = [String]()
+        array =  self.components(separatedBy: " ")
+        return array
+        }
+    }
+
+var arrayString = "1 2 3 4 5 ersssssss tr6 h6tg y6th ".lines()
+
+for i in arrayString {
+    print(i)
 }
