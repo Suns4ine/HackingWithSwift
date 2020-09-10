d3edb6fd83b93af41f86994c56984815e51d6445
Suns4ine <pronin.slavok@mail.ru>
2020-09-10 14:57:16 +0300

    New Consolidation 9: Extension

---
+++ Classic/Consolidation 9.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIView {
+    func bounceOut(duration: TimeInterval) {
+        UIView.animate(withDuration: duration) {
+            self.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+        }
+    }
+}
