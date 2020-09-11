ef0b8bed28d9a931a2779a356d8a52c9598532ba
Suns4ine <pronin.slavok@mail.ru>
2020-09-11 23:31:16 +0300

    New Project 25: Social

---
+++ Classic/Project 25/Project 25/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 25
+//
+//  Created by Vyacheslav Pronin on 11/09/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
