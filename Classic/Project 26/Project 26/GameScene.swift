de415836239031f69f6a37977454e561acf74b3a
Suns4ine <pronin.slavok@mail.ru>
2020-09-15 10:26:11 +0300

    fix bug for FatalError (letter = S)

--- Classic/Project 26/Project 26/GameScene.swift
+++ Classic/Project 26/Project 26/GameScene.swift
@@ -19,7 +19,12 @@ class GameScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        loadLevel()
     }
     
     func loadLevel() {
@@ -29,27 +34,55 @@ class GameScene: SKScene {
         guard let levelString = try? String(contentsOf: levelURL) else {
             fatalError("Could not load level1.txt from the app bundle.")
         }
-        
+
         let lines = levelString.components(separatedBy: "\n")
-        
+
         for (row, line) in lines.reversed().enumerated() {
-            for (colum, letter) in line.enumerated() {
-                let position = CGPoint(x: (64 * colum) + 32, y: (64 * colum) + 32)
+            for (column, letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
                 
                 if letter == "x" {
                     let node = SKSpriteNode(imageNamed: "block")
                     node.position = position
-                    
+
                     node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
                     node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
                     node.physicsBody?.isDynamic = false
                     addChild(node)
                 } else if letter == "v" {
-                    
-                } else if letter == "S" {
-                    
+                    let node = SKSpriteNode(imageNamed: "vortex")
+                    node.name = "vortex"
+                    node.position = position
+                    node.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 1)))
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+
+                    node.physicsBody?.categoryBitMask = CollisionTypes.vortex.rawValue
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    addChild(node)
+                } else if letter == "s" {
+                    let node = SKSpriteNode(imageNamed: "star")
+                    node.name = "star"
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+
+                    node.physicsBody?.categoryBitMask = CollisionTypes.star.rawValue
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    node.position = position
+                    addChild(node)
                 } else if letter == "f" {
-                    
+                    let node = SKSpriteNode(imageNamed: "finish")
+                    node.name = "finish"
+                    node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+                    node.physicsBody?.isDynamic = false
+
+                    node.physicsBody?.categoryBitMask = CollisionTypes.finish.rawValue
+                    node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+                    node.physicsBody?.collisionBitMask = 0
+                    node.position = position
+                    addChild(node)
                 } else if letter == " " {
                     
                 } else {
