445aa2fe703cda17d79e4997265edbe3c5717772
Suns4ine <pronin.slavok@mail.ru>
2020-09-16 20:14:39 +0300

    New Project 27: the Sandbox

---
+++ Classic/Project 27/Project 27/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Project 27
+//
+//  Created by Vyacheslav Pronin on 16/09/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+
+    @IBAction func redrawTapped(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
