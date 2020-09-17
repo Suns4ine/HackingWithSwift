57f12bf96e8b3c36b77ed0926ac3bf5f99b2d14e
Suns4ine <pronin.slavok@mail.ru>
2020-09-17 11:00:42 +0300

    Add funcs drawRectangle, drawCircle and drawCheckerboard

--- Classic/Project 27/Project 27/ViewController.swift
+++ Classic/Project 27/Project 27/ViewController.swift
@@ -10,15 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentDrawType = 0
     @IBOutlet var imageView: UIImageView!
 
     @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        case 1:
+            drawCircle()
+        case 2:
+            drawCheckerboard()
+        default:
+            break
+        }
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        drawRectangle()
     }
 
+    func drawRectangle() {
+        let render = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = render.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = img
+    }
 
+    func drawCircle() {
+        let render = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = render.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 502, height: 502).insetBy(dx: 5, dy: 5)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = img
+    }
+    
+    func drawCheckerboard() {
+        let render = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = render.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.black.cgColor)
+            
+            for row in 0..<8 {
+                for col in 0..<8 {
+                    if (row + col) % 2 == 0 {
+                        ctx.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+        imageView.image = img
+    }
 }
 
