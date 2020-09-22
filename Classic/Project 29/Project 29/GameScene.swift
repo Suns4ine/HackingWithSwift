52ce6a9c4239a11460b27330b6c36cb52437cdc4
Suns4ine <pronin.slavok@mail.ru>
2020-09-22 23:43:48 +0300

    New Project 29: Game Gorillas

---
+++ Classic/Project 29/Project 29/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project 29
+//
+//  Created by Vyacheslav Pronin on 22/09/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+}
