ec05c2ec579bc14ec6b728bbc903b69037fc60fd
Suns4ine <pronin.slavok@mail.ru>
2020-09-25 21:22:59 +0300

    Add func createBuildings

--- Classic/Project 29/Project 29/GameScene.swift
+++ Classic/Project 29/Project 29/GameScene.swift
@@ -16,11 +16,30 @@ enum CollisionTypes: UInt32 {
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+//    private var label : SKLabelNode?
+//    private var spinnyNode : SKShapeNode?
+    
+    var buildings = [BuildingNode]()
     
     override func didMove(to view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
         
+        createBuildings()
+    }
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
         
+        while currentX < 1024 {
+            let size = CGSize(width: Int.random(in: 2...4) * 40, height: Int.random(in: 300...600))
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: UIColor.red, size: size)
+            building.position = CGPoint(x: currentX - (size.width / 2), y: size.height / 2)
+            building.setup()
+            addChild(building)
+            
+            buildings.append(building)
+        }
     }
 }
