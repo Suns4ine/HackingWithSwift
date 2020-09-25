06e17212d99b122d2169119c0bf9b2013f38553c
Suns4ine <pronin.slavok@mail.ru>
2020-09-25 21:13:49 +0300

    Add funcs drawBuilding, configurePhysic and setup

--- Classic/Project 29/Project 29/GameScene.swift
+++ Classic/Project 29/Project 29/GameScene.swift
@@ -8,13 +8,13 @@
 
 import SpriteKit
 
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 4
+}
+
 class GameScene: SKScene {
-    
-    enum CollisionTypes: UInt32 {
-        case banana = 1
-        case building = 2
-        case player = 4
-    }
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
