7df58690b1579ee613b5fdb074339e19ba7c9fff
Suns4ine <pronin.slavok@mail.ru>
2020-09-30 23:43:27 +0300

    New Consolidation: game Concentration

---
+++ Classic/Consolidation 12/Consolidation 12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Consolidation 12
+//
+//  Created by Vyacheslav Pronin on 30/09/2020.
+//  Copyright © 2020 Vyacheslav Pronin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
