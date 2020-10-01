ac79812bf784d6e55e149ab387125909967ac195
Suns4ine <pronin.slavok@mail.ru>
2020-10-01 23:31:01 +0300

    Add Array LAbles

--- Classic/Consolidation 12/Consolidation 12/ViewController.swift
+++ Classic/Consolidation 12/Consolidation 12/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelArray: [UILabel]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
